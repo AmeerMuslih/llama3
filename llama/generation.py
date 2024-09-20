2ffc05bb67e898390e856d45ad5fa01fc41d2219
ameermuslih <ameermuslih@microsoft.com>
2024-09-20 18:04:28 +0300

    .

--- llama/generation.py
+++ llama/generation.py
@@ -161,9 +161,10 @@ class Llama:
 
         min_prompt_len = min(len(t) for t in prompt_tokens)
         max_prompt_len = max(len(t) for t in prompt_tokens)
+        print(f'Min prompt len: {min_prompt_len}, max prompt len: {max_prompt_len}')
         assert max_prompt_len <= params.max_seq_len
         total_len = min(params.max_seq_len, max_gen_len + max_prompt_len)
-
+        print(f'Max gen len: {max_gen_len}, total len: {total_len}')
         pad_id = self.tokenizer.pad_id
         tokens = torch.full((bsz, total_len), pad_id, dtype=torch.long, device=device)
         for k, t in enumerate(prompt_tokens):
