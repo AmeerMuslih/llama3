c32c982c5cd784cfabc327aab6ebad692a7f7418
ameermuslih <ameermuslih@microsoft.com>
2024-09-21 03:34:30 +0300

    .

--- run_SA.sh
+++ run_SA.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 for dir in ~/Ameer/llama3/Matrices/*/; do
-    nohup srun python3 ./Matmul_SA.py $dir > ./Outputs/output_$dir & disown
+    dirname=$(basename "$dir")
+    nohup srun python3 ./Matmul_SA.py $dir > ./Outputs/output_$dirname & disown
 done
