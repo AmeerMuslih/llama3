9bb9b8936d8d21b7f4c236df560992879f1de92c
ameermuslih <ameermuslih@microsoft.com>
2024-09-21 03:41:10 +0300

    fix the run_SA.sh

--- run_SA.sh
+++ run_SA.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-for dir in ~/Ameer/llama3/Matrices/*/; do
+start_dir=$1
+num_dirs=$2
+
+# Get a list of all directories and limit the range
+dirs=($(ls -d ~/Ameer/llama3/Matrices/*/))
+
+# Iterate over the specified range of directories
+for ((i=$start_dir; i<$((start_dir + num_dirs)); i++)); do
+    if [ $i -ge ${#dirs[@]} ]; then
+        echo "Reached end of directories"
+        break
+    fi
+
+    dir=${dirs[$i]}
     dirname=$(basename "$dir")
-    nohup srun python3 ./Matmul_SA.py $dir > ./Outputs/output_$dirname & disown
+    echo "Processing $dir"
+    nohup srun python3 ./Matmul_SA.py "$dir" > ./Outputs/output_$dirname & disown
 done
